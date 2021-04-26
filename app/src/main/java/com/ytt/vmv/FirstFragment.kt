668140708266b0fe10f94ac3b2230385426aa2af
Yip, Alex (UG - Computer Science) <ty00208@surrey.ac.uk>
2021-04-26 18:33:36 +0100

    Configured vote list page

--- app/src/main/java/com/ytt/vmv/FirstFragment.kt
+++ app/src/main/java/com/ytt/vmv/FirstFragment.kt
@@ -1,34 +1,93 @@
-package com.ytt.vmv;
+package com.ytt.vmv
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.graphics.*
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Transformation
+import kotlin.math.min
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
+data class Candidate(val name: String, val picUrl: String)
 
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+        val layoutManager = LinearLayoutManager(context)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.list_choices)
+
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = ChoicesAdapter(Array(10) { i -> Candidate("$i", "https://picsum.photos/500") })
+    }
+}
+
+class ChoicesAdapter(private val dataSet: Array<Candidate>) : RecyclerView.Adapter<ChoicesAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.text1)
+        val imageView: ImageView = view.findViewById(R.id.image_profile_pic)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.list_card_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = dataSet[position].name
+
+        Picasso.get()
+                .load(dataSet[position].picUrl)
+                .resize(60, 60)
+                .centerCrop()
+                .transform(CircleTransform())
+                .into(holder.imageView)
+    }
+
+    override fun getItemCount() = dataSet.size
+}
+
+class CircleTransform : Transformation {
+    override fun transform(source: Bitmap): Bitmap {
+        val size = min(source.width, source.height)
+        val x = (source.width - size) / 2
+        val y = (source.height - size) / 2
+        val squaredBitmap = Bitmap.createBitmap(source, x, y, size, size)
+        if (squaredBitmap != source) {
+            source.recycle()
+        }
+        val bitmap = Bitmap.createBitmap(size, size, source.config)
+        val canvas = Canvas(bitmap)
+        val paint = Paint()
+        val shader = BitmapShader(squaredBitmap,
+                Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+        paint.shader = shader
+        paint.isAntiAlias = true
+        val r = size / 2f
+        canvas.drawCircle(r, r, r, paint)
+        squaredBitmap.recycle()
+        return bitmap
+    }
+
+    override fun key(): String {
+        return "circle"
     }
 }
