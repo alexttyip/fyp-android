7b55d491f16cc7fad2c07869b80373530dd02eb2
Yip, Alex (UG - Computer Science) <ty00208@surrey.ac.uk>
2021-05-09 20:50:20 +0100

    Use ViewModel, add progress indicator for when list is null

--- app/src/main/java/com/ytt/vmv/VoteFragment.kt
+++ app/src/main/java/com/ytt/vmv/VoteFragment.kt
@@ -8,16 +8,12 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.databinding.BindingAdapter
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.google.android.material.progressindicator.CircularProgressIndicator
 import com.ytt.vmv.databinding.FragmentVoteBinding
 import com.ytt.vmv.databinding.ListCardItemBinding
 import com.ytt.vmv.models.VoteOption
-
-private val mockData = List(4) {
-    VoteOption(
-        "$it",
-        "https://picsum.photos/id/${it * 10}/200"
-    )
-}
+import com.ytt.vmv.models.VoteOptions
 
 class VoteFragment : Fragment() {
     override fun onCreateView(
@@ -26,14 +22,30 @@ class VoteFragment : Fragment() {
     ): View {
         val binding = FragmentVoteBinding.inflate(inflater, container, false)
 
-        binding.voteOptions = mockData
+        val voteOptions: VoteOptions by viewModels()
+
+        binding.voteOptions = voteOptions
+
+        binding.lifecycleOwner = this
 
         return binding.root
     }
 }
 
 @BindingAdapter("voteOptions")
-fun setVoteOptions(parent: LinearLayout, options: List<VoteOption>) {
+fun setVoteOptions(parent: LinearLayout, options: List<VoteOption>?) {
+    if (options == null) {
+        val indicator = CircularProgressIndicator(parent.context)
+
+        indicator.layoutParams = LinearLayout.LayoutParams(50, 50)
+        indicator.isIndeterminate = true
+
+        parent.addView(indicator)
+        return
+    }
+
+    parent.removeAllViews()
+
     options.forEach {
         val itemBinding = ListCardItemBinding.inflate(
             parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater,
--- app/src/main/java/com/ytt/vmv/models/VoteOptions.kt
+++ app/src/main/java/com/ytt/vmv/models/VoteOptions.kt
@@ -2,10 +2,32 @@ package com.ytt.vmv.models
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.squareup.picasso.Picasso
 import com.ytt.vmv.CircleTransform
 
 data class VoteOption(val name: String, val picUrl: String)
+
+class VoteOptions : ViewModel() {
+    private val options: MutableLiveData<List<VoteOption>> by lazy {
+        loadVoteOptions()
+    }
+
+    fun getVoteOptions(): LiveData<List<VoteOption>> {
+        return options
+    }
+
+    private fun loadVoteOptions(): MutableLiveData<List<VoteOption>> {
+        return MutableLiveData(List(4) {
+            VoteOption(
+                "$it",
+                "https://picsum.photos/id/${it * 10}/200"
+            )
+        })
+    }
+}
 
 @BindingAdapter("imageUrl")
 fun setImageUrl(imageView: ImageView, picUrl: String) {
